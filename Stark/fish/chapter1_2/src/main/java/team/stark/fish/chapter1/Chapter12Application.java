a77cda7950dfc0b3178ffe2db891cf966a10cb79
fish <1976669832@qq.com>
2019-07-14 17:15:18 +0800

    fish commit chapter1-2

---
+++ Stark/fish/chapter1_2/src/main/java/team/stark/fish/chapter1/Chapter12Application.java
@@ -0,0 +1,14 @@
+package team.stark.fish.chapter1;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter12Application.class, args);
+	}
+
+}
