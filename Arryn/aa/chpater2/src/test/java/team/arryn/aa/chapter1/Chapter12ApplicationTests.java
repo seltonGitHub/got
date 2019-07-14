b4b0211e87fe058a5bb3ccd290368648dd708972
aa <456646331@qq.com>
2019-07-15 07:58:26 +0800

    读取配置文件...

--- Arryn/aa/chpater2/src/test/java/team/arryn/aa/chapter1/Chapter12ApplicationTests.java
+++ Arryn/aa/chpater2/src/test/java/team/arryn/aa/chapter1/Chapter12ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Chapter1ApplicationTests {
+public class Chapter12ApplicationTests {
 
 	@Test
 	public void contextLoads() {
