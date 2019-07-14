091689a5304231df231e5f0cff13931983e31af7
aa <456646331@qq.com>
2019-07-14 23:27:12 +0800

    读取配置文件的两种方式

---
+++ Arryn/aa/chpater2/src/main/java/team/arryn/aa/chapter1/controller/HelloController.java
@@ -0,0 +1,13 @@
+package team.arryn.aa.chapter1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+   @RequestMapping("hello")
+    public String helloWorld() {
+        return "hello Spring boot";
+    }
+
+}
