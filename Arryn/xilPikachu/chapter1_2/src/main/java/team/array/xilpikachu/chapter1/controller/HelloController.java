008c312cab00b9bf5b3fb8073d33e0251881e139
xilPikachu <lixin771025@163.com>
2019-07-15 02:35:43 +0800

    获取配置文件的值 xilPikachu

--- Arryn/xilPikachu/chapter1_2/src/main/java/team/array/xilpikachu/chapter1/controller/HelloController.java
+++ Arryn/xilPikachu/chapter1_2/src/main/java/team/array/xilpikachu/chapter1/controller/HelloController.java
@@ -1,13 +1,37 @@
 package team.array.xilpikachu.chapter1.controller;
 
+import org.omg.CORBA.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${group}")
+    private String group;
+    @Value("${creater}")
+    private String creater;
+    @Value("${message}")
+    private String message;
+
     @RequestMapping("hello")
     public String hello() {
         return "hello world";
+    }
+
+    @RequestMapping("seeGroupConfig")
+    public String seeGroupConfig() {
+        return group;
+    }
+    @RequestMapping("seeCreaterConfig")
+    public String seeCreaterConfig() {
+        return creater;
+    }
+    @RequestMapping("seeMessageConfig")
+    public String seeMessageConfig() {
+        return message;
     }
 }
