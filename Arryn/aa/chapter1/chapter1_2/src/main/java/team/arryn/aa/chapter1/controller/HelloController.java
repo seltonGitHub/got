e7dc28d5490086ac39ad8e3d85929a7c9be93f19
aa <456646331@qq.com>
2019-07-14 23:16:06 +0800

    读取配置文件的两种方式

--- Arryn/aa/chapter1/chapter1_2/src/main/java/team/arryn/aa/chapter1/controller/HelloController.java
+++ Arryn/aa/chapter1/chapter1_2/src/main/java/team/arryn/aa/chapter1/controller/HelloController.java
@@ -1,12 +1,29 @@
 package team.arryn.aa.chapter1.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("hello")
-    public String HelloController(){
-        return "hello spring assistant";
+
+    @Value("${message}")
+    private String msg;
+    @Value("${group}")
+    private String group;
+    @Value("${create}")
+    private String creater;
+
+    @RequestMapping("seeMessageConfig")
+    public String seeMessage(){
+        return msg;
+    }
+    @RequestMapping("seeGroupConfig")
+    public String seeGroup(){
+        return group;
+    }
+    @RequestMapping("seeCreaterConfig")
+    public String seeCreater(){
+        return creater;
     }
 }
--- chapter1_2/src/main/java/team/targaryen/selton/chapter1/Chapter12Application.java
+++ chapter1_2/src/main/java/team/targaryen/selton/chapter1/Chapter12Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Chapter12Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Chapter12Application.class, args);
 	}
 
