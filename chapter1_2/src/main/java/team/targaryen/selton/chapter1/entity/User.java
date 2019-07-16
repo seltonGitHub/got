2c229250afe9eb6b8a32c380f699b72028f41930
Selton <seltonzyf@outlook.com>
2019-07-16 11:12:21 +0800

    enhancement #5 init chapter3-1-1 构建一个较为复杂的RESTful API.

---
+++ chapter1_2/src/main/java/team/targaryen/selton/chapter1/entity/User.java
@@ -0,0 +1,17 @@
+package team.targaryen.selton.chapter1.entity;
+
+public class User {
+    private int id;
+
+    // other field
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // other get and set method
+}
