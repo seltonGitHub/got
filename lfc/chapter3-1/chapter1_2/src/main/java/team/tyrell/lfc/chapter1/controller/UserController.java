8482f481f499b12304464a4a97ed46ad0ae11ad3
lfc <2817279418@qq.com>
2019-07-20 23:12:01 +0800

    enhancenment #7

--- lfc/chapter3-1/chapter1_2/src/main/java/team/tyrell/lfc/chapter1/controller/UserController.java
+++ lfc/chapter3-1/chapter1_2/src/main/java/team/tyrell/lfc/chapter1/controller/UserController.java
@@ -1,6 +1,7 @@
 package team.tyrell.lfc.chapter1.controller;
 
 import org.springframework.web.bind.annotation.*;
+import team.tyrell.lfc.chapter1.VO.ResponseInfo;
 import team.tyrell.lfc.chapter1.entity.User;
 
 import java.util.HashMap;
@@ -12,32 +13,34 @@ import java.util.Map;
 public class UserController {
     private static Map<Integer, User> userMap = new HashMap<>();
 
+
     @PostMapping("add")
-    public String addUser(@RequestBody User user) {
+    public ResponseInfo addUser(@RequestBody User user) {
         userMap.put(user.getId(), user);
-        return "ok";
+        return new ResponseInfo("ok");
     }
 
     @GetMapping("{id}")
-    public User userGetById(@PathVariable("id") int id) {
-        User user = userMap.get(id);
-        System.out.println("user = " + user);
-        return userMap.get(id);
+    public ResponseInfo userGetById(@PathVariable("id") int id) throws Exception  {
+//        throw new Exception("a exception occur");
+       /* User user = userMap.get(id);
+        System.out.println("user = " + user);*/
+        return new ResponseInfo(userMap.get(id));
     }
 
     @DeleteMapping("{id}")
-    public int removeUser(@PathVariable("id") int id) {
+    public ResponseInfo removeUser(@PathVariable("id") int id) {
         //成功删除的用户数量
         int successRemoveNum = 0;
         userMap.remove(id);
-        return ++successRemoveNum;
+        return new ResponseInfo(++successRemoveNum);
     }
 
     @PutMapping("{id}")
-    public int modifyUser(@PathVariable("id") int id,@RequestBody User user) {
+    public ResponseInfo modifyUser(@PathVariable("id") int id,@RequestBody User user) {
         //成功修改的用户数量
         int successModifyNum = 0;
         userMap.replace(user.getId(), user);
-        return successModifyNum++;
+        return new ResponseInfo(successModifyNum++);
     }
 }
